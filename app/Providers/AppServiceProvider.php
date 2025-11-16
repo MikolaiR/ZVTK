e6d49ea55d7b104dd5180985387181f6f6156861
MikolaiR <mikalairnn@gmail.com>
2025-11-16 20:56:42 +0300

    fix: add if debug for force scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if(config('app.debug') === false) {
+            URL::forceScheme('https');
+        }
         JsonResource::withoutWrapping();
     }
 }
